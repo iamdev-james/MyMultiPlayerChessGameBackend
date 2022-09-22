7a7e495d5932fd26689b97ef731f295f5a6c862b
iamdev-james <jamesoluwabukola87@gmail.com>
2022-09-22 09:29:47 -0100

    Set up a virtual room for the game room

--- GameLogic.js
+++ GameLogic.js
@@ -41,7 +41,8 @@
      // register event listeners for video chat app:
      videoChatBackend()
  }
- 
+
+//  Setting up video chat connection here
  function videoChatBackend() {
   // main function listeners
   gameSocket.on("callUser", (data) => {
@@ -51,4 +52,43 @@
   gameSocket.on("acceptCall", (data) => {
       io.to(data.to).emit('callAccepted', data.signal);
   })
+}
+
+function playerJoinsGame(idData) {
+  /**
+   * Joins the given socket to a session with it's gameId
+   */
+
+  // A reference to the player's Socket.IO socket object
+  var sock = this
+  
+  // Look up the room ID in the Socket.IO manager object.
+  var room = io.sockets.adapter.rooms[idData.gameId]
+ // console.log(room)
+
+  // If the room exists...
+  if (room === undefined) {
+      this.emit('status' , "This game session does not exist." );
+      return
+  }
+  if (room.length < 2) {
+      // attach the socket id to the data object.
+      idData.mySocketId = sock.id;
+
+      // Join the room
+      sock.join(idData.gameId);
+
+      console.log(room.length)
+
+      if (room.length === 2) {
+          io.sockets.in(idData.gameId).emit('start game', idData.userName)
+      }
+
+      // Emit an event notifying the clients that the player has joined the room.
+      io.sockets.in(idData.gameId).emit('playerJoinedRoom', idData);
+
+  } else {
+      // Otherwise, send an error message back to the player.
+      this.emit('status' , "There are already 2 people playing in this room." );
+  }
 }
