67f2ad1b78c32a0aa3bd64c7f0ed7b64306ca2a8
iamdev-james <jamesoluwabukola87@gmail.com>
2022-09-22 09:32:55 -0100

    Added requestusername, recievedusername and disconnect listeners

--- GameLogic.js
+++ GameLogic.js
@@ -1,104 +1,135 @@
-
-
 /**
- * Here is where we should register event listeners and emitters. 
+ * Here is where we should register event listeners and emitters.
  */
 
- var io
- var gameSocket
- // gamesInSession stores an array of all active socket connections
- var gamesInSession = []
- 
- 
- const initializeGame = (sio, socket) => {
-     /**
-      * initializeGame sets up all the socket event listeners. 
-      */
- 
-     // initialize global variables.
-     io = sio 
-     gameSocket = socket 
- 
-     // pushes this socket to an array which stores all the active sockets.
-     gamesInSession.push(gameSocket)
- 
-     // Run code when the client disconnects from their socket session. 
-     gameSocket.on("disconnect", onDisconnect)
- 
-     // Sends new move to the other socket session in the same room. 
-     gameSocket.on("new move", newMove)
- 
-     // User creates new game room after clicking 'submit' on the frontend
-     gameSocket.on("createNewGame", createNewGame)
- 
-     // User joins gameRoom after going to a URL with '/game/:gameId' 
-     gameSocket.on("playerJoinGame", playerJoinsGame)
- 
-     gameSocket.on('request username', requestUserName)
- 
-     gameSocket.on('recieved userName', recievedUserName)
- 
-     // register event listeners for video chat app:
-     videoChatBackend()
- }
+var io;
+var gameSocket;
+// gamesInSession stores an array of all active socket connections
+var gamesInSession = [];
+
+const initializeGame = (sio, socket) => {
+	/**
+	 * initializeGame sets up all the socket event listeners.
+	 */
+
+	// initialize global variables.
+	io = sio;
+	gameSocket = socket;
+
+	// pushes this socket to an array which stores all the active sockets.
+	gamesInSession.push(gameSocket);
+
+	// Run code when the client disconnects from their socket session.
+	gameSocket.on("disconnect", onDisconnect);
+
+	// Sends new move to the other socket session in the same room.
+	gameSocket.on("new move", newMove);
+
+	// User creates new game room after clicking 'submit' on the frontend
+	gameSocket.on("createNewGame", createNewGame);
+
+	// User joins gameRoom after going to a URL with '/game/:gameId'
+	gameSocket.on("playerJoinGame", playerJoinsGame);
+
+	gameSocket.on("request username", requestUserName);
+
+	gameSocket.on("recieved userName", recievedUserName);
+
+	// register event listeners for video chat app:
+	videoChatBackend();
+};
 
 //  Setting up video chat connection here
- function videoChatBackend() {
-  // main function listeners
-  gameSocket.on("callUser", (data) => {
-      io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
-  })
+function videoChatBackend() {
+	// main function listeners
+	gameSocket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("hey", {
+			signal: data.signalData,
+			from: data.from,
+		});
+	});
 
-  gameSocket.on("acceptCall", (data) => {
-      io.to(data.to).emit('callAccepted', data.signal);
-  })
+	gameSocket.on("acceptCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal);
+	});
 }
 
 // Trigger when a player join game
 function playerJoinsGame(idData) {
-  /**
-   * Joins the given socket to a session with it's gameId
-   */
+	/**
+	 * Joins the given socket to a session with it's gameId
+	 */
 
-  // A reference to the player's Socket.IO socket object
-  var sock = this
-  
-  // Look up the room ID in the Socket.IO manager object.
-  var room = io.sockets.adapter.rooms[idData.gameId]
- // console.log(room)
+	// A reference to the player's Socket.IO socket object
+	var sock = this;
 
-  // If the room exists...
-  if (room === undefined) {
-      this.emit('status' , "This game session does not exist." );
-      return
-  }
-  if (room.length < 2) {
-      // attach the socket id to the data object.
-      idData.mySocketId = sock.id;
+	// Look up the room ID in the Socket.IO manager object.
+	var room = io.sockets.adapter.rooms[idData.gameId];
+	// console.log(room)
 
-      // Join the room
-      sock.join(idData.gameId);
+	// If the room exists...
+	if (room === undefined) {
+		this.emit("status", "This game session does not exist.");
+		return;
+	}
+	if (room.length < 2) {
+		// attach the socket id to the data object.
+		idData.mySocketId = sock.id;
 
-      console.log(room.length)
+		// Join the room
+		sock.join(idData.gameId);
 
-      if (room.length === 2) {
-          io.sockets.in(idData.gameId).emit('start game', idData.userName)
-      }
+		console.log(room.length);
 
-      // Emit an event notifying the clients that the player has joined the room.
-      io.sockets.in(idData.gameId).emit('playerJoinedRoom', idData);
+		if (room.length === 2) {
+			io.sockets.in(idData.gameId).emit("start game", idData.userName);
+		}
 
-  } else {
-      // Otherwise, send an error message back to the player.
-      this.emit('status' , "There are already 2 people playing in this room." );
-  }
+		// Emit an event notifying the clients that the player has joined the room.
+		io.sockets.in(idData.gameId).emit("playerJoinedRoom", idData);
+	} else {
+		// Otherwise, send an error message back to the player.
+		this.emit("status", "There are already 2 people playing in this room.");
+	}
 }
 
+// When a user wants to create a new game instance
 function createNewGame(gameId) {
-    // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
-    this.emit('createNewGame', {gameId: gameId, mySocketId: this.id});
+	// Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
+	this.emit("createNewGame", { gameId: gameId, mySocketId: this.id });
 
-    // Join the Room and wait for the other player
-    this.join(gameId)
+	// Join the Room and wait for the other player
+	this.join(gameId);
 }
 
+// Track users movement
+function newMove(move) {
+	/**
+	 * First, we need to get the room ID in which to send this message.
+	 * Next, we actually send this message to everyone except the sender
+	 * in this room.
+	 */
+
+	const gameId = move.gameId;
+
+	io.to(gameId).emit("opponent move", move);
+}
+
+// When a user disconnect from the game
+function onDisconnect() {
+	var i = gamesInSession.indexOf(gameSocket);
+	gamesInSession.splice(i, 1);
+}
+
+// Request username
+function requestUserName(gameId) {
+	io.to(gameId).emit("give userName", this.id);
+}
+
+// Username Recieved?
+function recievedUserName(data) {
+	data.socketId = this.id;
+	io.to(data.gameId).emit("get Opponent UserName", data);
+}
+
+exports.initializeGame = initializeGame;
